e0254718c28c71bd0d9875294ce18269e5e1f458
Erik Torres Puente <81220576+ErikTP@users.noreply.github.com>
2021-04-16 14:00:05 +0200

    ProjectUpdate

---
+++ src/main/java/com/example/projecteriktp/ProjectErikTpApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecteriktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectErikTpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectErikTpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projecteriktp/ProjectErikTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecteriktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectErikTpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
